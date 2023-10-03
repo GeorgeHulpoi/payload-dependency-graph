96452ba2c2a49e9ee65163ae4aa4b795835d4520
George-Valentin Hulpoi <george.v.hulpoi@gmail.com>
2023-10-03 20:34:17 +0300

    feat: exporting dependency graph

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import plugin from './plugin';
 import service from './service';
 
+export * from './dependency-graph';
 export * from './types';
 export * from './subject';
 export * from './subscription';
