1f921b16bb8021d123dfcb28363305fefedf778a
George-Valentin Hulpoi <george.v.hulpoi@gmail.com>
2023-10-03 11:30:06 +0300

    feat: exports types, subject and subscription

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import plugin from './plugin';
 import service from './service';
 
-export { Subject } from './subject';
-export { Subscription } from './subscription';
+export * from './types';
+export * from './subject';
+export * from './subscription';
 export const DependencyGraphService = service;
 export const DependencyGraphPlugin = plugin;
