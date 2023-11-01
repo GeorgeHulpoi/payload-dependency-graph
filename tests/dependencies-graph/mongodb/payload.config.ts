1bd56972539a20f41071998a65b05a2757da6b97
George-Valentin Hulpoi <george.v.hulpoi@gmail.com>
2023-11-01 21:38:03 +0200

    test: tests for mongodb dependency graph

---
+++ tests/dependencies-graph/mongodb/payload.config.ts
@@ -0,0 +1,40 @@
+import path from 'path';
+
+import { mongooseAdapter } from '@payloadcms/db-mongodb';
+import { slateEditor } from '@payloadcms/richtext-slate';
+import { buildConfig } from 'payload/config';
+
+import { DependencyGraphPlugin, MongoDBDependencyGraph } from '../../../src';
+import collections from '../../dev/collections';
+import globals from '../../dev/globals';
+import { seed } from '../../dev/seed';
+
+export const editorExtractor = jest.fn((args) => {});
+
+export default buildConfig({
+	serverURL: 'http://localhost:3000',
+	admin: {
+		disable: true,
+	},
+	collections,
+	globals,
+	typescript: {
+		outputFile: path.resolve(__dirname, 'payload-types.ts'),
+	},
+	graphQL: {
+		disable: true,
+	},
+	db: mongooseAdapter({
+		url: process.env.MONGODB_URI!,
+	}),
+	editor: slateEditor({}),
+	plugins: [
+		DependencyGraphPlugin({
+			editorExtractor,
+			factory: () => new MongoDBDependencyGraph(),
+		}),
+	],
+	onInit: async (payload) => {
+		await seed(payload);
+	},
+});
