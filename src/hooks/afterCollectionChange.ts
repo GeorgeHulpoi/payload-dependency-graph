96a3a798717d91ffcaea40a855251e9bb7dd9971
George-Valentin Hulpoi <george.v.hulpoi@gmail.com>
2023-11-03 12:58:31 +0200

    fix: closes https://github.com/GeorgeHulpoi/payload-dependency-graph/issues/9

--- src/hooks/afterCollectionChange.ts
+++ src/hooks/afterCollectionChange.ts
@@ -4,13 +4,20 @@ import DependencyGraphService from '../service';
 
 const afterCollectionChange: (collection: string) => CollectionAfterChangeHook =
 	(collection: string) =>
-	({ doc, previousDoc, operation }) => {
-		return DependencyGraphService.onCollectionChange({
-			doc,
-			previousDoc,
-			operation,
-			collection,
-		});
+	({ doc, previousDoc, operation, req, context }) => {
+		const { draft = false } = context;
+		const shouldSaveDraft = Boolean(
+			draft && req.payload.collections[collection].config.versions.drafts,
+		);
+
+		if (!shouldSaveDraft) {
+			return DependencyGraphService.onCollectionChange({
+				doc,
+				previousDoc,
+				operation,
+				collection,
+			});
+		}
 	};
 
 export default afterCollectionChange;
